26c3c2d1909df30c556fa85b0d2d9431206cfc41
manuel09 <manu.17@live.it>
2025-05-06 22:40:37 +0200

    Update StreamingCommunity.kt
    update url streamingcommunity

--- StreamingCommunity/src/main/kotlin/it/dogior/hadEnough/StreamingCommunity.kt
+++ StreamingCommunity/src/main/kotlin/it/dogior/hadEnough/StreamingCommunity.kt
@@ -42,7 +42,7 @@ class StreamingCommunity : MainAPI() {
                 "X-Inertia" to true.toString(),
                 "X-Inertia-Version" to inertiaVersion
             ).toMutableMap()
-        val mainUrl = "https://streamingcommunity.spa"
+        val mainUrl = "https://streamingcommunity.education"
         var name = "StreamingCommunity"
     }
 
